5035b9c15e52aa57fb7b03f9bb850909737970bf
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2017-11-13 11:51:17 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pilihanawal;
+
+int main()
+{
+	puts("BATTLE OF OLYMPIA");
+	puts("1. New Game");
+	puts("2. Load Game");
+	printf("Masukkan pilihan : ");
+	scanf("%d", pilihanawal);
+	switch (pilihanawal) {
+		case 1 : 
+	}
+}
